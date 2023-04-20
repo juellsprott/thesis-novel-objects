e14d1748d7ced63c21f8318c901b1d9466ae1d9e
juellsprott <73904000+juellsprott@users.noreply.github.com>
2023-04-20 16:17:47 +0200

    got working pipeline

---
+++ pipeline.py
@@ -0,0 +1,22 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+import torch
+
+raw_image = Image.open("data/NOUN-2-600DPI/2007.jpg").convert("RGB")
+
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+
+# there are several ways to load the model:
+# model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b") # load in float32
+# model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16) # load in float16
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", device_map="auto", load_in_8bit=True) # load in int8
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+inputs = processor(raw_image, return_tensors="pt").to(device, torch.float16)
+
+generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+print(generated_text)
