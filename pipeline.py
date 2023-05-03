c6c8302d640bae047fd6e9e162f5c5417a26d4f1
juellsprott <juell.sprott@student.uva.nl>
2023-05-04 00:27:16 +0200

    update dataset and fixed pipeline for home usage

--- pipeline.py
+++ pipeline.py
@@ -1,23 +1,42 @@
-import requests
 from PIL import Image
 from transformers import AutoProcessor, Blip2ForConditionalGeneration
 import torch
+import os
+import csv
+from tqdm import tqdm
 
-raw_image = Image.open("data/NOUN-2-600DPI/2007.jpg").convert("RGB")
+
 
 
 processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
 
-# there are several ways to load the model:
-# model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b") # load in float32
-# model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16) # load in float16
-model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", device_map="auto", load_in_8bit=True) # load in int8
+# load in float16 # load in int8
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", load_in_8bit=True, device_map="auto")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-inputs = processor(raw_image, return_tensors="pt").to(device, torch.float16)
 
-generated_ids = model.generate(**inputs, max_new_tokens=20)
-print(generated_ids)
-generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
-print(generated_text)
+# Read data from input file into memory
+data = []
+with open('dataset.csv', 'r') as csvinput:
+    reader = csv.reader(csvinput, delimiter=',')
+    # Add new column name to header row
+    header = next(reader)
+    header.append('BLIP-2 descriptions')
+    data.append(header)
+    # Add new column data to remaining rows
+    for row in tqdm(reader):
+        # Use data from one column to generate data for new column
+        if len(row) > 0:
+
+            raw_image = Image.open(row[0]).convert("RGB")
+            inputs = processor(raw_image, return_tensors="pt").to(device, torch.float16)
+
+            generated_ids = model.generate(**inputs, max_new_tokens=20)
+            generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+            data.append(row + [generated_text])
+
+# Write updated data with new column back to input file
+with open('dataset.csv', 'w') as csvoutput:
+    writer = csv.writer(csvoutput)
+    writer.writerows(data)
